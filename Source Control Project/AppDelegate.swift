796e1be17a9f2d1a5936d2958847b325b70d1f9e
Chris Norris <53133897+chrisnorristech@users.noreply.github.com>
2020-12-11 06:47:19 -0600

    Initial Commit

---
+++ Source Control Project/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Source Control Project
+//
+//  Created by Christopher Norris on 12/11/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Source Control Project/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Source Control Project
+//
+//  Created by Christopher Norris on 12/11/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
